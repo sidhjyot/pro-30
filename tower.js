2dbcada32a44d8c6b32157282d50db250e1ab7fe
sidhjyot <65850342+sidhjyot@users.noreply.github.com>
2020-08-11 17:27:53 +0530

    Have fixed the Block class

--- tower.js
+++ tower.js
@@ -1,58 +1,37 @@
-class Box{
-    constructor(x, y) {
+class Box {
+  constructor(x, y, width, height) {
+      var options = {
+         
+          restitution :0.4,
+          friction :0.0,
+          //isStatic:true
+         
+      }
+      this.visibility = 225;
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
       
-        this.body = Bodies.rectangle(x, y,40,60);
-        this.Visiblity = 255;
-        this.image=loadImage("tower.jpg")
-        World.add(world, this.body); 
-      }
-      display(){
-        if(this.body.speed < 3){
-          var angle = this.body.angle;
-          push();
-          translate(this.body.position.x, this.body.position.y);
-          rotate(angle);
-          rectMode(CENTER);
-          fill (118,233,255);
-          image(this.image, this.body.position.x, this.body.position.y, 50, 50);
-          pop();
-          
-        }
-
-        else{
-
-          World.remove(world, this.body);
-          push();
-          this.Visiblity = this.Visiblity - 20;
-          tint(255,this.Visiblity);
-          image(this.image, this.body.position.x, this.body.position.y, 50, 50);
-          pop();
-        }      
-      }
+    }
+   
+    display(){
+      console.log(this.body.speed);
+      if(this.body.speed <3){
+      var angle = this.body.angle;
+      var pos= this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0,0,this.width, this.height);
+      pop();
+    }else{
+      World.remove(world, this.body);
+      push();
+      this.visibility = this.visibility -5;
+      pop();
+      
+    }
+  }
 }
-
-// console.log(this.body.speed);
- /*if(this.body.speed < 3){
-  super.display();
- }
- else{
-   World.remove(world, this.body);
-   push();
-   this.Visiblity = this.Visiblity - 5;
-   tint(255,this.Visiblity);
-   image(this.image, this.body.position.x, this.body.position.y, 50, 50);
-   pop();
- }
- /* else{
-          World.remove(world, this.body);
-          push();
-          this.Visiblity = this.Visiblity - 5;
-          tint(255,this.Visiblity);
-          rect(0, 0, this.width,this.height);
-          pop();
-        } 
-
-
-
-
-};*/
