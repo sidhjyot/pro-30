8a9eb936696490a50ed03d5a8098f24ad398da20
sidhjyot <65850342+sidhjyot@users.noreply.github.com>
2020-08-11 17:30:10 +0530

    Please check with the positions of the blocks

--- sketch.js
+++ sketch.js
@@ -27,39 +27,53 @@ function setup() {
   
  
   ground1= new Ground(600,690,width,30);
-  ground2= new Ground(1000,400,290,30);
-  ground3= new Ground(600,500,350,30);
-
+  ground2= new Ground(390,300,250,10);
+  ground3= new Ground(700,200,200,10);
   
-  box1= new Box(895+20,70 );
-  box2= new Box(935+20,70 );
-	box3= new Box(975+20,70 );
-  box4= new Box(1035,70 ); 
-  box5= new Box(1055+20,70 );
-	box6= new Box(935+20,50 );
-	box7= new Box(975+20,50 );
-  box8= new Box(1035,50 );
-  box9= new Box(975+20,30 );
+
+  block1 = new Box(300,275,30,40);
+  block2 = new Box(330,275,30,40);
+  block3 = new Box(360,275,30,40);
+  block4 = new Box(390,275,30,40);
+  block5 = new Box(420,275,30,40);
+  block6 = new Box(450,275,30,40);
+  block7 = new Box(480,275,30,40);
+  //level two
+  block8 = new Box(330,235,30,40);
+  block9 = new Box(360,235,30,40);
+  block10 = new Box(390,235,30,40);
+  block11 = new Box(420,235,30,40);
+  block12 = new Box(450,235,30,40);
   
-  box10= new Box(485,90 );
-  box11= new Box(525,90 );
-  box12= new Box(565,90 );
-  box13= new Box(605,90 );
-  box14= new Box(645,90 );
-  box15= new Box(685,90 );
-  box16= new Box(725,90 );
+//   box1= new Box(895+20,70 );
+//   box2= new Box(935+20,70 );
+// 	box3= new Box(975+20,70 );
+//   box4= new Box(1035,70 ); 
+//   box5= new Box(1055+20,70 );
+// 	box6= new Box(935+20,50 );
+// 	box7= new Box(975+20,50 );
+//   box8= new Box(1035,50 );
+//   box9= new Box(975+20,30 );
+  
+//   box10= new Box(485,90 );
+//   box11= new Box(525,90 );
+//   box12= new Box(565,90 );
+//   box13= new Box(605,90 );
+//   box14= new Box(645,90 );
+//   box15= new Box(685,90 );
+//   box16= new Box(725,90 );
 
-  box17= new Box(525,70 );
-  box18= new Box(565,70 );
-  box19= new Box(605,70 );
-  box20= new Box(645,70 );
-  box21= new Box(685,70 );
+//   box17= new Box(525,70 );
+//   box18= new Box(565,70 );
+//   box19= new Box(605,70 );
+//   box20= new Box(645,70 );
+//   box21= new Box(685,70 );
 
-  box22= new Box(565,50 );
-  box23= new Box(605,50 );
-  box24= new Box(645,50 );
+//   box22= new Box(565,50 );
+//   box23= new Box(605,50 );
+//   box24= new Box(645,50 );
 
-  box25= new Box(605,30 );
+//   box25= new Box(605,30 );
   
   
   polygon1 = Bodies.circle(50,200,20);
@@ -86,30 +100,31 @@ function draw() {
   ground2.display();
   ground3.display();
   box1.display();
-  box2.display();
-  box3.display();
-  box4.display();
-  box5.display();
-  box6.display();
-  box7.display();
-  box8.display();
-  box9.display();
-  box10.display();
-  box11.display();
-  box12.display();
-  box13.display();
-  box14.display();
-  box15.display();
-  box16.display();
-  box17.display();
-  box18.display();
-  box19.display();
-  box20.display();
-  box21.display();
-  box22.display();
-  box23.display();
-  box24.display();
-  box25.display();
+   block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+//   box13.display();
+//   box14.display();
+//   box15.display();
+//   box16.display();
+//   box17.display();
+//   box18.display();
+//   box19.display();
+//   box20.display();
+//   box21.display();
+//   box22.display();
+//   box23.display();
+//   box24.display();
+//   box25.display();
 
   imageMode(CENTER)
   image(polygon_img,polygon1.position.x,polygon1.position.y,40,40);
